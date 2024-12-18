167cf21bb7bcf043db584d3d16610112ddd4f33f
Corentin Recanzone <corentindevpro@hotmail.com>
2024-12-18 22:52:32 +0100

    fix: fixed documentation and name

--- app/enums/user_roles.ts
+++ app/enums/user_roles.ts
@@ -1,8 +1,18 @@
-export enum user_roles {
-  SUPER_ADMIN = 'super_admin', // Créateur de la plateforme
-  ADMIN = 'admin', // Gérant d’un compte ou d’une entreprise
-  APP_MANAGER = 'app_manager', // Gestionnaire d’applications
-  MARKETING = 'marketing', // Responsable marketing
-  SUPPORT = 'support', // Assistance client
-  COMMERCIAL = 'commercial', // Responsable commercial
+/**
+ * Enumération des rôles utilisateur
+ * @enum {string} user_roles
+ * @property {string} SUPER_ADMIN - Créateur de la plateforme
+ * @property {string} ADMIN - Gérant d’un compte ou d’une entreprise
+ * @property {string} APP_MANAGER - Gestionnaire d’applications
+ * @property {string} MARKETING - Responsable marketing
+ * @property {string} SUPPORT - Assistance client
+ * @property {string} COMMERCIAL - Responsable commercial
+ */
+export enum UserRoles {
+  SUPER_ADMIN = 'super_admin',
+  ADMIN = 'admin',
+  APP_MANAGER = 'app_manager',
+  MARKETING = 'marketing',
+  SUPPORT = 'support',
+  COMMERCIAL = 'commercial',
 }
