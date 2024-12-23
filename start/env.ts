0a89b4df57cc613ae7343b19f367b076353aa313
Corentin Recanzone <corentindevpro@hotmail.com>
2024-12-23 17:01:17 +0100

    fix: fixed missing var env

--- start/env.ts
+++ start/env.ts
@@ -22,7 +22,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string(),
+  DB_DATABASE_NAME: Env.schema.string(),
   DB_DEBUG: Env.schema.boolean(),
   HEALTH: Env.schema.string(),
   AWS_ACCESS_KEY_ID: Env.schema.string(),
