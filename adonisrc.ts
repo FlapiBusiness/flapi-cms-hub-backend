0c85e59369e536075b98d5af050f37813af5219f
Corentin Recanzone <corentindevpro@hotmail.com>
2025-01-10 15:54:14 +0100

    feat: add package tuyaux and config

--- adonisrc.ts
+++ adonisrc.ts
@@ -14,6 +14,7 @@ export default defineConfig({
     () => import('@adonisjs/core/commands'),
     () => import('@adonisjs/lucid/commands'),
     () => import('@adonisjs/mail/commands'),
+    () => import('@tuyau/core/commands'),
   ],
 
   /*
@@ -38,6 +39,7 @@ export default defineConfig({
     () => import('@adonisjs/auth/auth_provider'),
     () => import('@adonisjs/core/providers/edge_provider'),
     () => import('@adonisjs/mail/mail_provider'),
+    () => import('@tuyau/core/tuyau_provider'),
   ],
 
   /*
