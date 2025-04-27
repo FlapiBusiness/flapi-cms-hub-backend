97e9bbca6d9f754cabdaab1003b2c40ace55de1b
greglmx0 <113339984+greglmx0@users.noreply.github.com>
2025-04-27 23:46:20 +0200

    feat: add user application event log listener for NATS integration

--- start/events.ts
+++ start/events.ts
@@ -3,7 +3,13 @@ import SignInEvent from '#events/signin_event'
 import SignUpEvent from '#events/signup_event'
 import SignOutEvent from '#events/signout_event'
 const ApplicationEventLogListener = () => import('#listeners/application_event_log_listener')
+const UserApplicationEventLogListener = () => import('#listeners/user_application_envent_log_listener')
 
 emitter.listen(SignInEvent, [ApplicationEventLogListener])
 emitter.listen(SignUpEvent, [ApplicationEventLogListener])
 emitter.listen(SignOutEvent, [ApplicationEventLogListener])
+
+emitter.listen(SignInEvent, [UserApplicationEventLogListener])
+emitter.listen(SignUpEvent, [UserApplicationEventLogListener])
+emitter.listen(SignOutEvent, [UserApplicationEventLogListener])
+emitter.listen('user:application_event_log', [UserApplicationEventLogListener])
